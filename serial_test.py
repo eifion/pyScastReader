55e22313b02b0ece321391c01a6efa632c720950
Eifion <eifion@gmail.com>
2012-12-19 17:02:48 +0000

    Now uses python-daemon to run in the background.

--- serial_test.py
+++ serial_test.py
@@ -1,35 +1,41 @@
 from scast_buffer import *
+import daemon
 import glob
 import time
 import serial
 
-matching_units = glob.glob('/dev/serial/by-id/*')
-if matching_units:
-  PORT_NAME = matching_units[0]
-  print "USB device found: " + PORT_NAME
-else:
-  print "No devices found, exiting."
-  exit()
+
+def find_stick_and_start_reading_from_it():
+  matching_units = glob.glob('/dev/serial/by-id/*')
+  if matching_units:
+    PORT_NAME = matching_units[0]
+    print "USB device found: " + PORT_NAME
+  else:
+    print "No devices found, exiting."
+    exit()
 
 
-BAUD_RATE = 19200
+  BAUD_RATE = 19200
 
-ser = serial.Serial(
-  port = PORT_NAME,
-  baudrate = BAUD_RATE,
-  parity = serial.PARITY_NONE,
-  stopbits = serial.STOPBITS_ONE,
-  bytesize = serial.EIGHTBITS)
+  ser = serial.Serial(
+    port = PORT_NAME,
+    baudrate = BAUD_RATE,
+    parity = serial.PARITY_NONE,
+    stopbits = serial.STOPBITS_ONE,
+    bytesize = serial.EIGHTBITS)
 
-# Getting serial port
-# Port is at /dev/serial/by-id/<something>
-# use:
-# import glob
-# glob.glob('/dev/serial/by-id/*')
 
-buffer = ScastBuffer()
-while 1:
-  while ser.inWaiting() > 0:
-    chars = ser.read(1).encode('hex')
-    buffer.add(chars)   
-  time.sleep(1) 
+  buffer = ScastBuffer()
+  while 1:
+    while ser.inWaiting() > 0:
+      chars = ser.read(1).encode('hex')
+      buffer.add(chars)   
+    time.sleep(1)
+
+def run():
+  with daemon.DaemonContext():
+    find_stick_and_start_reading_from_it() 
+
+
+if __name__== "__main__":
+  run()
