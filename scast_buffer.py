7eabfd3f462837b4c7c804e4c1251530e283f83a
= <eifion@gmail.com>
2012-12-24 16:43:23 +0000

    Changed the way the buffer works to stop potential errors when the reading data contains 0D0A.

--- scast_buffer.py
+++ scast_buffer.py
@@ -10,19 +10,25 @@ class ScastBuffer:
     
   def add(self, char):
     self.buffer += char
-    self.process_buffer()
+    self.processself.buffer()
     
-  def process_buffer(self):
-    s = self.buffer.find(self.SCAST_TOKEN)
-    if s == -1:
-      return
-    
-    e = self.buffer.find(self.CR_LF, s)
-    if e == -1:
-      return
-    
-    scast = Scast(self.buffer[s:e])
-    self.buffer = self.buffer[e+4:]
-      
-    
-  
+  def processself.buffer(self):
+	  while True:
+  		start = self.buffer.find(SCAST_TOKEN)
+  		if start == -1:
+    		break
+
+  		self.buffer = self.buffer[start:]
+ 			if len(self.buffer) < READING_COUNT_END:
+    		break
+
+  		reading_data_length = int(self.buffer[READING_COUNT_START:READING_COUNT_END].decode('hex'), 16)
+  		reading_count = (reading_data_length - 6) / 4
+
+  		scast_length = READING_DATA_START + (READING_DATA_LENGTH * reading_count)
+  		if len(self.buffer) < scast_length:
+    		break
+
+  		scast = self.buffer[:scast_length]
+  		self.buffer = self.buffer[scast_length:]
+
