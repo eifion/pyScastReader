7c4c4e3ed645dc9aff1473e6497491bc924d336c
= <eifion@gmail.com>
2012-12-24 17:09:20 +0000

    Fixes for errors caused by mixing tabs and spaces.

--- scast_buffer.py
+++ scast_buffer.py
@@ -13,22 +13,22 @@ class ScastBuffer:
     self.process_buffer()
     
   def process_buffer(self):
-	  while True:
-  		start = self.buffer.find(SCAST_TOKEN)
-  		if start == -1:
-    		break
+    while True:
+      start = self.buffer.find(SCAST_TOKEN)
+      if start == -1:
+        break
 
-  		self.buffer = self.buffer[start:]
- 			if len(self.buffer) < READING_COUNT_END:
-    		break
+      self.buffer = self.buffer[start:]
+      if len(self.buffer) < READING_COUNT_END:
+        break
 
-  		reading_data_length = int(self.buffer[READING_COUNT_START:READING_COUNT_END].decode('hex'), 16)
-  		reading_count = (reading_data_length - 6) / 4
+      reading_data_length = int(self.buffer[READING_COUNT_START:READING_COUNT_END].decode('hex'), 16)
+      reading_count = (reading_data_length - 6) / 4
 
-  		scast_length = READING_DATA_START + (READING_DATA_LENGTH * reading_count)
-  		if len(self.buffer) < scast_length:
-    		break
+      scast_length = READING_DATA_START + (READING_DATA_LENGTH * reading_count)
+      if len(self.buffer) < scast_length:
+        break
 
-  		scast = self.buffer[:scast_length]
-  		self.buffer = self.buffer[scast_length:]
+      scast = self.buffer[:scast_length]
+      self.buffer = self.buffer[scast_length:]
 
