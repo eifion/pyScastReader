23564505a46b04a534602f484b3d97ea1f901b98
Eifion Bedford <eifion@asciicasts.com>
2013-07-05 17:36:04 +0100

    Now looks for serial port in different places depending on OS.

--- serial_test.py
+++ serial_test.py
@@ -1,10 +1,20 @@
-import daemon
+#import daemon
 import glob
+import platform
 from serial_port import *
 
 
 def find_stick_and_start_reading_from_it():
-  matching_units = glob.glob('/dev/ttyUSB*')
+  platform_name = platform.system().lower()
+  if platform_name == 'darwin':
+    unit_pattern = 'tty.SLAB_USBtoUART'
+  elif platform_name == 'linux':
+    unit_pattern = 'ttyUSB*'
+  else:
+    print "Unsure where to look for serial port on {}".format(platform_name)
+    exit()
+
+  matching_units = glob.glob('/dev/{}'.format(unit_pattern))
   if matching_units:
     port_name = matching_units[0]
     print "USB device found: " + port_name
@@ -14,8 +24,8 @@ def find_stick_and_start_reading_from_it():
     exit()
 
 def run():
-  with daemon.DaemonContext():
-    find_stick_and_start_reading_from_it() 
+  #with daemon.DaemonContext():
+  find_stick_and_start_reading_from_it() 
 
 if __name__== "__main__":
   run()
